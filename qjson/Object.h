09ec6d4d1b8302d9a370815af03876d83d25602a
Stacy Smith <stacyqsmity@gmail.com>
2021-07-07 12:51:13 -0500

    Updated copyright to GPL.

--- qjson/Object.h
+++ qjson/Object.h
@@ -1,9 +1,31 @@
-﻿#pragma once
+﻿/*
+ * QJson Library
+ * https://github.com/kit10us/QJson
+ * Copyright (c) 2002, Kit10 Studios LLC
+ *
+ * This file is part of QJson Library (a.k.a. QJson)
+ *
+ * QJson is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QJson is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QJson.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
 #include <qjson/Value.h>
 #include <qjson/Pair.h>
 #include <qjson/IteratorTemplate.h>
 #include <list>
 
+ /// See http://json.org/ for implementation details.
 namespace qjson
 {
 	class Object : public Value
